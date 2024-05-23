2cf4f53586618658295dddfbbc3ec56aa3a728d1
Dhruv Dipak Pawar <89768718+DHRUVPAWAR12@users.noreply.github.com>
2024-05-23 23:08:02 +0530

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,24 +1,77 @@
-import React from 'react'
+import React, { useState } from "react";
+import Add from "../Images/addAvatar.png";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth, storage } from "../firebase";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { doc, setDoc } from "firebase/firestore";
 
-const Register = () => {
+
+const Register = () => { 
+  const [err, setErr] = useState(false);
+  const handleSubmit = async (e) => { 
+    e.preventDefault();
+    const displayName = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+    const file = e.target[3].files[0];
+try {
+      const res = await createUserWithEmailAndPassword(auth,email,password);
+      
+const storageRef = ref(storage, displayName);
+
+const uploadTask = uploadBytesResumable(storageRef, file);
+
+uploadTask.on(
+  (error) => {
+    setErr(true);
+  }, 
+  () => {
+    getDownloadURL(uploadTask.snapshot.ref).then(async(downloadURL) => {
+      await updateProfile(res.user,{
+        displayName,
+        photoURL: downloadURL,
+      });
+      await setDoc(doc(db, "users", res.user.uid), {
+        uid: res.user.uid,
+        displayName,
+        email,
+        photoURL: downloadURL,
+      });
+    });
+  }
+);
+
+}catch(err){
+  setErr(true);
+}
+  
+};
+  
+  
+
   return (
-    <div className='formContainer'>
-        <div className='formWrapper'>
-            <span className='logo'>Chat Application</span>
-            <span className='title'>Register</span>
-            <form>
-                <input type="text" placeholder='Display Name'/>
-                <input type="email" placeholder='email'/>
-                <input type="password" placeholder='password'/>
-                <input type="file" />
-                <button>Sign Up</button>
-            </form>
-            <p>You do have an account? Login</p>
-            <button>Sign Up</button>
-        </div>
-
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">Lama Chat</span>
+        <span className="title">Register</span>
+        <form onSubmit={handleSubmit}>
+          <input required type="text" placeholder="display name" />
+          <input required type="email" placeholder="email" />
+          <input required type="password" placeholder="password" />
+          <input required style={{ display: "none" }} type="file" id="file" />
+          <label htmlFor="file">
+            <img src={Add} alt="" />
+            <span>Add an avatar</span>
+          </label>
+          <button>Sign up</button>
+          {err && <span>Something went horrible!!!</span>}
+        </form>
+        <p>
+          You do have an account? Login
+        </p>
+      </div>
     </div>
   );
-}
+};
 
-export default Register
+export default Register;
