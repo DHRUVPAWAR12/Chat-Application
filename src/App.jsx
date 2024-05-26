e5369ff528976ce3e5738243d30123a814a90494
Dhruv Dipak Pawar <89768718+DHRUVPAWAR12@users.noreply.github.com>
2024-05-26 23:17:39 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,44 @@
-import Register from "./pages/Register";
-import "./style.scss"
+import List from './components/list/list';
+import Chat from './components/chat/chat';
+import Details from './components/details/details';
+import Login from './components/Login/Login';
+import Notification from './components/notification/notification';
+import { useEffect } from 'react';
+import { auth } from './lib/firebase';
+import { useUserStore } from './lib/userStore';
+import { onAuthStateChanged } from 'firebase/auth';
 
-function App(){
+const App = () =>{
+  const {currentUser,isLoading,fetchUserInfo} = useUserStore();
+ 
+  useEffect(() =>{
+    const unSub = onAuthStateChanged(auth, (user) =>{
+      fetchUserInfo(user,uid)
+
+    });
+
+  return () =>{
+    unSub();
+    
+  };
+ },[fetchUserInfo]);
+ console.log(currentUser);
+ if(isLoading) return <div className="loading">Loading...</div>
   return (
-    <Register/>
-  );
+    <div className="container">
+      {
+        currentUser ?(
+          <>
+          <List/>
+          <Chat/>
+          <Details/>
+          </>
+        ) : (
+          <Login/>
+        )
+      }
+        <Notification />
+    </div>
+  )
 }
-export default App;
+export default App
