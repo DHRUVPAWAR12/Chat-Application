09124a9bdc3eb9ac99fca9052f314366a5a9dedf
Dhruv Dipak Pawar <89768718+DHRUVPAWAR12@users.noreply.github.com>
2024-05-28 23:39:42 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,19 @@ import Chat from './components/chat/chat';
 import Details from './components/details/details';
 import Login from './components/Login/Login';
 import Notification from './components/notification/notification';
-import { useEffect } from 'react';
+import { useEffect, uid} from 'react';
 import { auth } from './lib/firebase';
 import { useUserStore } from './lib/userStore';
 import { onAuthStateChanged } from 'firebase/auth';
+import { chatStore } from './lib/chatStore';
 
 const App = () =>{
   const {currentUser,isLoading,fetchUserInfo} = useUserStore();
+  const {chatId} = chatStore();
  
   useEffect(() =>{
-    const unSub = onAuthStateChanged(auth, (user) =>{
-      fetchUserInfo(user,uid)
-
+    const unSub = onAuthStateChanged(auth, (user) => {
+      fetchUserInfo(user?.uid);
     });
 
   return () =>{
@@ -22,19 +23,20 @@ const App = () =>{
     
   };
  },[fetchUserInfo]);
- console.log(currentUser);
- if(isLoading) return <div className="loading">Loading...</div>
+console.log(currentUser);
+ if(isLoading) return <div className="loading"> Loading...</div>;
+  
   return (
     <div className="container">
       {
-        currentUser ?(
+        currentUser ? (
           <>
           <List/>
-          <Chat/>
-          <Details/>
+          {chatId && <Chat/>}
+          {chatId && <Details/>}
           </>
         ) : (
-          <Login/>
+          <Login />
         )
       }
         <Notification />
